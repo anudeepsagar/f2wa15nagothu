a99a7c80b43a8d00d8ee43b84a65085c0803092d
anudeepsagar <68767594+anudeepsagar@users.noreply.github.com>
2022-10-26 21:27:24 -0500

    updated users file in js bonus

--- routes/computation.js
+++ routes/computation.js
@@ -20,33 +20,13 @@ var y = Math.random();
 
 
 var val1="Math.pow(base, exponent) applied to "+ x +"and"+ y +" is "+ Math.pow(x, y)
-var val2="Math.log(x) applied to "+ x + " is "+ Math.log(x)
-
-
-
-var val3= "Math.log10(x) applied to "+ x + " is "+ Math.log10(x);
-var val4= "Math.log10(x) applied to "+ x + " is "+ Math.log10(x);
-
-
-
-
+var val2="Math.sign(x)applied to "+ x + " is "+ Math.sign(x)
+var val3="Math.tanh(x) applied to "+ x + " is "+ Math.tanh(x);
 
 /* GET home page. */
 
-
-
 router.get('/', function(req, res, next) {
-
-
-
-  res.render('computation', { title:'ABDUL RAHEEM SYED   ',value1: val1, value2:vasl2, value3:val3, value4:val4 });
-
-
-
+  res.render('computation', { title:'ANUDEEP SAGAR NAGOTHU  ',value1: val1, value2:vasl2, value3:val3, value4:val4 });
 });
-
-
-
-
 
 module.exports = router;
