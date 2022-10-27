cd125199af3a40795208c64fbede75c30bf69537
anudeepsagar <68767594+anudeepsagar@users.noreply.github.com>
2022-10-26 22:37:16 -0500

    bonus now

--- routes/computation.js
+++ routes/computation.js
@@ -1,23 +1,12 @@
 var express = require('express');
 
-
-
 var router = express.Router();
-
-
 
 var x = Math.random();
 
 
 
 var y = Math.random();
-
-
-
-
-
-
-
 
 var val1="Math.cos()applied to "+ x + " is "+ Math.cos(x)
 var val2="Math.asin()applied to "+ x + " is "+ Math.asin(x)
@@ -26,7 +15,7 @@ var val3="Math.asinh() applied to "+ x + " is "+ Math.asinh(x);
 /* GET home page. */
 
 router.get('/', function(req, res, next) {
-  res.render('computation', { title:'ANUDEEP SAGAR NAGOTHU  ',value1: val1, value2:vasl2, value3:val3, value4:val4 });
+  res.render('computation', { title:'ANUDEEP SAGAR NAGOTHU  ',value1: val1, value2:val2, value3:val3});
 });
 
 module.exports = router;
